e16995cb1ec0efcf4772571baece975f1fc04cfe
Eleni Partakki <47921764+epartakki@users.noreply.github.com>
2021-10-22 08:17:07 -0400

    Add files via upload

---
+++ CaeserCipher.py
@@ -0,0 +1,60 @@
+## Name:  Eleni Partakki
+## Peer: N/A
+## Resources: Notes given in assignment: [C = (P+k)mod26], [C = (P-k)mod26]
+
+
+# Helpful: the key is the number of places to shift each character.
+# This number could be specified numerically (e.g., 4).
+# Assume the plaintext spaces are only uppercase alphabetic letters (ex. A, B, C, D, … , Z)
+
+
+#(2 points) Create encryption function to encrypt a message using a key [C = (P+k)mod26]
+
+def encrypt(message, key):
+
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    caeser = ""
+
+    for letter in message:
+        if letter in alphabet:
+            letter_index = (alphabet.find(letter) + key) % 26
+
+            caeser = caeser+alphabet[letter_index]
+    return caeser
+
+print("~~~Welcome to the Caesar Cipher. Begin by encrypting a message!~~~\n~~~Note: everything will turn to uppercase~~~")
+print("")
+message = input("Enter message to encrypt: ")
+
+#Makes everything uppercase to avoid lowercase cases
+message = message.upper()
+
+k = int(input("Enter Caesar shift number: "))
+print("Encrypted Message: ", encrypt(message, k))
+
+
+
+#(2 points) Create decryption function to decrypt a ciphertext using a key[C = (P-k)mod26]
+
+def decrypt(message, key):
+
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    caeser = ""
+
+    for letter in message:
+        if letter in alphabet:
+            letter_index = (alphabet.find(letter) - key) % 26
+
+            caeser = caeser+alphabet[letter_index]
+    return caeser
+
+print("")
+print("~~~Now, you can attempt to decrypt a message!~~~")
+print("")
+message = input("Enter message to decrypt: ")
+
+#Makes everything uppercase to avoid lowercase cases
+message = message.upper()
+
+k = int(input("Enter Caesar shift number: "))
+print("Decrypted Message: ", decrypt(message, k))
